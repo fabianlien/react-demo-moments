9d2a56bffe0a012e9bec44dbae6e7ef75eaa606b
Fabian Lien <fabian.lien@gmail.com>
2022-06-08 11:12:28 +0000

    07 create axios interceptors for requests and responses

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://dj-r-f-api-moments.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = tru
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
