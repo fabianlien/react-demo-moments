33ca306a685db38d4f5baa9d4943d67c5aabc260
Fabian Lien <fabian.lien@gmail.com>
2022-06-08 13:41:11 +0000

    09 create hook to handle click toggle events and apply to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,14 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 
 const NavBar = () => {
-  const setCurrentUser = useSetCurrentUser()
+  const setCurrentUser = useSetCurrentUser();
   const currentUser = useCurrentUser();
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
+
 
   const handleSignOut = async () => {
     try {
@@ -50,7 +53,7 @@ const NavBar = () => {
   </>
 
   return (
-    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+    <Navbar expanded={expanded} expand="md" fixed="top" className={styles.NavBar}>
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -58,7 +61,7 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
